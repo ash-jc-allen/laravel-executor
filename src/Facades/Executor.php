455d73c2923c3383bfd48f00a5c1df61fdfed3e5
Ashley Allen <mail@ashallendesign.co.uk>
2020-02-08 02:43:32 +0000

    Added the facade method docblocks.

--- src/Facades/Executor.php
+++ src/Facades/Executor.php
@@ -4,6 +4,15 @@ namespace AshAllenDesign\LaravelExecutor\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string runArtisan(string $command)
+ * @method static string runExternal(array $command)
+ * @method static string getOutput()
+ * @method static \AshAllenDesign\LaravelExecutor\Classes\Executor resetOutput()
+ * @method static \AshAllenDesign\LaravelExecutor\Classes\Executor setOutput(string $output)
+ *
+ * @see \AshAllenDesign\LaravelExecutor\Classes\Executor
+ */
 class Executor extends Facade
 {
     /**
