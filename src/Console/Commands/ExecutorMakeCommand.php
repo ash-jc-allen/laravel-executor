b8aaa07e5b71c7fd504c03330503f10f30509c64
Ashley Allen <mail@ashallendesign.co.uk>
2020-06-08 22:03:21 +0100

    Updated executor command stub to run in console mode.

--- src/Console/Commands/ExecutorMakeCommand.php
+++ src/Console/Commands/ExecutorMakeCommand.php
@@ -191,7 +191,7 @@ class ExecutorMakeCommand extends GeneratorCommand
         );
 
         $find = "//";
-        $replaceWith = "(new {$executorClass}())->run();";
+        $replaceWith = "(new {$executorClass}())->run(true);";
 
         return str_replace($find, $replaceWith, $fileContents);
     }
