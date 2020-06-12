2f9efc04d66c1105931e35969cf9ac79965251d8
Ashley Allen <mail@ashallendesign.co.uk>
2020-06-12 15:44:19 +0100

    Added the functionality to display desktop notifications.

---
+++ src/Traits/DesktopNotifications.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AshAllenDesign\LaravelExecutor\Traits;
+
+use Joli\JoliNotif\Notification;
+use Joli\JoliNotif\NotifierFactory;
+use ReflectionClass;
+
+trait DesktopNotifications
+{
+    /**
+     * Add a desktop notification to the Executor queue.
+     *
+     * @param  Notification  $notification
+     * @return $this
+     */
+    public function desktopNotification(Notification $notification): self
+    {
+        $this->commandsToRun[] = $notification;
+
+        return $this;
+    }
+
+    /**
+     * Build a simple notification using just a title and
+     * a body. Then add it to the Executor queue for
+     * running.
+     *
+     * @param  string  $title
+     * @param  string  $body
+     * @return $this
+     */
+    public function simpleDesktopNotification(string $title, string $body): self
+    {
+        $notification = (new Notification())
+            ->setTitle($title)
+            ->setBody($body)
+            ->setIcon(__DIR__.'/path/to/your/icon.png');
+
+        $this->desktopNotification($notification);
+
+        return $this;
+    }
+
+    /**
+     * Display the desktop notification to the user.
+     *
+     * @param  Notification  $notification
+     */
+    private function displayNotification(Notification $notification): void
+    {
+        $notifier = NotifierFactory::create();
+
+        $notifier->send($notification);
+    }
+
+    /**
+     * A notification that can be displayed once the entire
+     * executor has been run.
+     *
+     * @return Notification
+     * @throws \ReflectionException
+     */
+    private function executorCompleteNotification(): Notification
+    {
+        $executorName = (new ReflectionClass($this))->getShortName();
+
+        return (new Notification())
+            ->setTitle('Executor complete!')
+            ->setBody('The '.$executorName.' executor has been run successfully.')
+            ->setIcon(__DIR__.'/path/to/your/icon.png');
+    }
+}
